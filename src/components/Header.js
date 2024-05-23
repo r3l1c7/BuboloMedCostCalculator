e9ac4b25152f20c2de8224bb60f9e712173e727c
John Doe <r3l1c7@gmail.com>
2024-05-23 05:28:02 -0400

    bug changes 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 //src\components\Header.js
-/*import React from 'react';
+import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 py-4">
+    <header className="bg-white-500 py-4">
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold text-white">Prescription Cost Calculator</h1>
       </div>
     </header>
   );
 };
 
-export default Header;*/
+export default Header;

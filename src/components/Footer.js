6e0ed2595bf1527a1c1c10f687a68b58425daaa9
John Doe <r3l1c7@gmail.com>
2024-05-23 05:28:26 -0400

    bug changes 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,13 @@
-// src/components/Footer.js
-/*
+//src\components\Footer.js
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-4 mt-8">
+    <footer className="bg-white-800 py-4 mt-8">
       <div className="container mx-auto px-4 text-center text-white">
-        &copy; {new Date().getFullYear()} Prescription Cost Calculator. All rights reserved.
       </div>
     </footer>
   );
 };
 
 export default Footer;
-*/

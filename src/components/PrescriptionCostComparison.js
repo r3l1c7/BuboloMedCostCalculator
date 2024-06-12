a47d641864639b56500bb61b6a912eee45a427b9
r3l1c7 <70303385+r3l1c7@users.noreply.github.com>
2024-06-12 16:34:21 -0400

    Update PrescriptionCostComparison.js

--- src/components/PrescriptionCostComparison.js
+++ src/components/PrescriptionCostComparison.js
@@ -1,7 +1,7 @@
 // src\components\PrescriptionCostComparison.js
 import React from 'react';
-import { Badge } from "@/components/ui/badge"
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 
 const PrescriptionCostComparison = ({ medication }) => {
   const retailPrice = medication.Retail_Price; // The retail price of the medication
@@ -21,12 +21,19 @@ const PrescriptionCostComparison = ({ medication }) => {
       <Card>
         <CardHeader>
           <CardTitle>Price Comparison</CardTitle>
-          <CardDescription><Badge variant="outline" className="text-green-900 bg-slate-000">{medication.Medication}</Badge></CardDescription>
+          <CardDescription><Badge variant="outline" className="text-green-900 bg-slate-100">{medication.Medication}</Badge></CardDescription>
         </CardHeader>
         <CardContent>
           <div className="flex justify-between items-center">
             <p className="text-xl font-medium my-2">Retail Price: <strong>{formatter.format(retailPrice)}</strong></p>
-            <p className="text-xl font-medium my-2">Our Price: <strong>{formatter.format(ourPrice)}</strong></p>
+            <p className="text-xl font-medium my-2">
+              Our Price: 
+              <strong>
+                <div className="inline-block text-white px-4 py-2 rounded cursor-not-allowed" style={{ backgroundColor: '#cd5928' }}>
+                  {formatter.format(ourPrice)}
+                </div>
+              </strong>
+            </p>
           </div>
           <div className="text-slate-900">
             <Badge className="text-green-700 bg-slate-200">You Save: {formatter.format(savings)}</Badge>

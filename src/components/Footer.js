a5c3ef54ab7cbd5c7bce9e705fec5120ae4f3fb6
r3l1c7 <70303385+r3l1c7@users.noreply.github.com>
2024-07-02 13:37:48 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-white-800 py-4 mt-8">
       <div className="container mx-auto px-4 text-center text-white">
+        <span>*Prices may vary.</span>
       </div>
     </footer>
   );
